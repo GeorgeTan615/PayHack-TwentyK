e523ccc26485dfee7cb0de353c7fa1d403fbdd6b
GeorgeTan615 <87482146+GeorgeTan615@users.noreply.github.com>
2023-05-28 01:27:51 +0800

    Time to solve conflicts

--- src/pages/pay1.tsx
+++ src/pages/pay1.tsx
@@ -3,14 +3,14 @@ import Numpad from "@/components/Numpad";
 import {useState} from 'react';
 const Pay1 = () =>{
 	const [focused, setFocused] = useState(false);
-	const [amount, setAmount] = useState(0);
+	const [amount, setAmount] = useState(0.00);
 
 	return (
 		<div className="flex flex-col h-screen justify-center p-7">
 			<div className="font-bold text-4xl">
 				Enter your amount (RM)
 			</div>
-			<input type="number" id="first_name" onFocus={()=>setFocused(true)} onBlur={()=>setFocused(false)} value={amount} onChange={(e)=>setAmount(parseFloat(e.target.value))} className="mt-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="0" required />
+			<input type="number" id="first_name" onFocus={()=>setFocused(true)} onBlur={()=>setFocused(false)} value={amount} onChange={(e)=>setAmount(parseFloat(parseFloat(e.target.value).toFixed(2)))} step="0.01" className="mt-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="0" required />
 			<Link href={{
 					pathname: '/pay2',
 					query: { amount: amount },
--- src/pages/pay2.tsx
+++ src/pages/pay2.tsx
@@ -1,13 +1,26 @@
 import { useRouter } from 'next/router'
-
+import ListBox from '@/components/ListBox';
 const Pay2 = () => {
 	const router = useRouter();
 	const {amount} = router.query;	
+	let roundedUpAmount:number = 0;
+	if (amount && !Array.isArray(amount)){
+		roundedUpAmount = parseFloat((Math.ceil(parseFloat(amount))-parseFloat(amount)).toFixed(2))
+	}
 	return(
-		<div className="flex flex-col h-screen justify-center p-7">
-			<div className="flex text-3xl">
-				You're paying&nbsp;<span className="font-bold">RM{amount}</span>
+		<div className="flex flex-col h-screen justify-between p-7">
+			<div>
+				<div className="flex text-3xl mb-4">
+					You're paying&nbsp;<span className="font-bold">RM{amount}</span>
+				</div>
+				<ListBox />
+				<div className="mt-4">
+					RM{roundedUpAmount} will be rounded up for insurance.
+				</div>
+
 			</div>
+			<button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Pay</button>
+
 		</div>
 	)
 
